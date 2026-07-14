2dceb3ab162071c826dcd5ff41c02ce55739a822
your_name <hariharan@g>
2026-07-14 12:50:19 +0530

    Optimize bundle size and code-splitting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('framer-motion')) {
+              return 'framer-motion';
+            }
+            if (id.includes('lucide-react')) {
+              return 'lucide-react';
+            }
+            if (id.includes('react')) {
+              return 'react-vendor';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
